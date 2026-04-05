ec7de52e62037eb3d21f5676abb0635c636d9b8b
Thyrak12 <thyrak9167@gmail.com>
2026-04-05 14:00:46 +0700

    s3 terraform

---
+++ terraform/s3.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "images" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
